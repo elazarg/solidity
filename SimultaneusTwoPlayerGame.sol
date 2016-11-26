6cb1f5e21ad37e78322cf00f880898fc981acdbb
Elazar Gershuni <elazarg@gmail.com>
2016-11-26 21:52:29 +0200

    Update and rename RPS.sol to SimultaneusTwoPlayerGame.sol

--- SimultaneusTwoPlayerGame.sol
+++ SimultaneusTwoPlayerGame.sol
@@ -1,12 +1,12 @@
 pragma solidity ^0.4.0;
 
-contract RPSI {
-    /* Incentive-based Rock-Scissors-Paper game:
-    0. OFFLINE:
-    0.1: Choose a secret number, whose value modulo-3 define whether it is a rock, a paper or scissors.
+contract SimultaneusTwoPlayerGameI {
+    /* Incentive-based simultaneus two player game:
+    0. OFFLINE: 
+    0.1: Choose a secret number.
     0.2: Get magic.token(secret).
     
-    1-2. deposit(token): Player-by-player commit to the token,
+    1-2. deposit(token): Player-by-player commit to the token, 
     3: Players query state until it is equal to REVEAL1 or REVEAL2.
          ONLY THEN should they put the secret anywhere accessible to miners or anyone.
          DO NOT automate it using a contract in any way, unless you KNOW it will only execute on the client.
@@ -27,14 +27,19 @@ contract RPSI {
 
 library magic {
     // Helper function. Should only run LOCALLY by clients.
-    function token(uint v) constant returns(bytes32) {
+    function commitment(uint v) constant returns(bytes32) {
         return sha3(v);
     }
 }
 
-contract RPS is RPSI {
-    uint256 constant PENALTY = 10 ether;
-    uint256 constant COST = 100 ether;
+contract SimultaneusTwoPlayerGame is SimultaneusTwoPlayerGameI {
+    
+    function judge(uint secret1, uint secret2) internal returns (uint index);
+    
+    function payment() constant internal returns (uint[3]);
+    
+    uint256 constant PENALTY = 0; // = 10 ether;
+    uint256 constant COST = 0; // = 100 ether;
 
     enum State { JOIN1, JOIN2,
                  REVEAL1, REVEAL2,
@@ -42,17 +47,17 @@ contract RPS is RPSI {
                  DONE }
 
     State state = State.JOIN1;
-    bytes32[2] tokens;
+    bytes32[2] commitments;
     mapping(bytes32 => address) players;
-    mapping(bytes32 => int8) choice;
+    mapping(bytes32 => uint) revealedSecret;
 
     function deposit(bytes32 token) payable validStates(State.JOIN1, State.JOIN2) {
         if (msg.value != COST + PENALTY)
             throw;
-        if (players[token] != address(0x0))
+        if (players[token] != 0)
             throw;
         players[token] = msg.sender;
-        tokens[uint(state)] = token;
+        commitments[uint(state)] = token;
     }
 
     // Convenience function; this.state can be queried directly
@@ -70,29 +75,21 @@ contract RPS is RPSI {
     // assume: if a real player calls this, mayReveal() returns true.
     // The _test_ for this assumption is too late.
     function reveal(uint secret) validStates(State.REVEAL1, State.REVEAL2) {
-        bytes32 t = magic.token(secret);
-        if (players[t] == 0 || choice[t] != 0)
+        bytes32 c = magic.commitment(secret);
+        if (players[c] == 0 || revealedSecret[c] != 0)
             throw;
-        choice[t] = int8(secret % 3) + 3;
+        revealedSecret[c] = secret;
     }
     
     function collect(bytes32 token) validStates(State.COLLECT1, State.COLLECT2) {
-        bytes32 opponentToken = (token == tokens[0]) ? tokens[1] : tokens[0];
-        uint result = gameResult(choice[token], choice[opponentToken]);
+        bytes32 opponentToken = (token == commitments[0]) ? commitments[1] : commitments[0];
+        uint index = judge(revealedSecret[token], revealedSecret[opponentToken]);
+        uint[3] memory p = payment();
         address player = players[token];
         delete players[token];
         // Reentrancy is fine
-        if (!player.send(result))
+        if (!player.send(payment()[index]))
             throw;
-    }
-    
-    function gameResult(int8 choice1, int8 choice2) internal returns (uint) {
-        int8 diff = (choice1 - choice2) % 3;
-        //                      this player  | other player
-        if (diff == 0) return COST + PENALTY + 0       + 0      ;
-        if (diff == 1) return COST + PENALTY + COST    + 0      ;
-        if (diff == 2) return 0    + 0       + 0       + PENALTY;
-        throw;
     }
 
     modifier validStates(State from, State to) {
@@ -108,13 +105,42 @@ contract RPS is RPSI {
     }
 
     function cleanup() internal {
-        delete choice[tokens[0]];
-        delete choice[tokens[1]];
+        delete revealedSecret[commitments[0]];
+        delete revealedSecret[commitments[1]];
 
         // These have been already cleaned, but we want to allow local reasoning:
-        delete players[tokens[0]];
-        delete players[tokens[1]];
+        delete players[commitments[0]];
+        delete players[commitments[1]];
 
-        delete tokens;
+        delete commitments;
+    }
+}
+
+contract RockPaperScissors is SimultaneusTwoPlayerGame {
+    function judge(uint secret1, uint secret2) internal returns (uint index) {
+        uint8 diff = uint8((secret1 % 3 - secret2 % 3) % 3);
+        if (diff == 0) return 0;
+        if (diff == 1) return 1;
+        if (diff == 2) return 2;
+        throw;
+    }
+    
+    function payment() constant internal returns (uint[3]) {
+        return [COST + COST + PENALTY,
+                COST + 0    + PENALTY,
+                0    + 0    + PENALTY];
+    }
+}
+
+contract OddsAndEvens is SimultaneusTwoPlayerGame {
+    function judge(uint secret1, uint secret2) internal returns (uint index) {
+        uint diff = (secret1 % 2 + secret2 % 2);
+        return (diff % 2 == 0) ? 1 : 2;
+    }
+    
+    function payment() constant internal returns (uint[3]) {
+        return [0,
+                COST + 0    + PENALTY,
+                0    + 0    + PENALTY];
     }
 }
