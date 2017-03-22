d795a9408fc6049f9a810ef3d56e5829e6d86b2d
Elazar Gershuni <elazarg@gmail.com>
2017-03-22 17:14:49 +0200

    Support general dependencies

--- Deps.sol
+++ Deps.sol
@@ -1,31 +1,4 @@
 
-contract Dependencies {
-    uint[5] deps;
-    bool[5] completed;
-    
-    event Completed(uint i);
-    
-    modifier depends_on0() {
-        _;
-    }
-    // TODO: depends_on(variable). requires some sort of optional type
-    modifier depends_on1(uint dep) {
-        require(completed[deps[dep]]);
-        _;
-    }
-    modifier depends_on2(uint dep1, uint dep2) {
-        require(completed[deps[dep1]]);
-        require(completed[deps[dep2]]);
-        _;
-    }
-    modifier id(uint i) {
-        require(!completed[i]);
-        _;
-        completed[i] = true;
-        Completed(i);
-    }
-}
-
 contract Roles {
     enum Role { PLAYER, HOST }
     mapping(address => Role) role;
@@ -36,58 +9,128 @@ contract Roles {
     }
 }
 
-contract Game is Roles, Dependencies { 
-    function open(uint value, uint commitment) {
-        require(sha3(value) == bytes32(commitment));
+contract Dependencies {
+    bool[256] completed;
+    function step(uint i) internal returns(bool) {
+        return completed[i]; 
     }
-    function open(int value, uint commitment) {
-        require(sha3(value) == bytes32(commitment));
+    event Completed(uint i);
+    
+    modifier first {
+        _;
+    }
+    modifier only_after(bool condition) {
+        require(condition);
+        _;
+    }
+    modifier perform_step(uint i) {
+        require(!step(i));
+        _;
+        completed[i] = true;
+        assert(step(i));
+        Completed(i);
     }
 }
 
-contract MontyHall is Game {
-    uint256 hidden_car;
-    int door1;
-    int goat;
-    int door2;
-    int car;
+contract GameDep is Roles, Dependencies { 
+    function open(uint value, uint commitment) internal constant returns(bool) {
+        return sha3(value) == bytes32(commitment);
+    }
+    function open(int value, uint commitment) internal constant returns(bool) {
+        return open(uint(value), commitment);
+    }
+}
 
-    function hide_car(uint256 commitment) by(Role.HOST) id(0) depends_on0() {
+contract MontyHallDep is GameDep {
+    uint256 hidden_car;
+    uint door1;
+    uint goat;
+    uint door2;
+    uint car;
+
+    function hide_car(uint256 commitment) perform_step(0) by(Role.HOST) first {
         hidden_car = commitment;
     }
-    function play_door1(int value) by(Role.PLAYER) id(1) depends_on1(0) {
+    function play_door1(uint value) perform_step(1) by(Role.PLAYER) only_after(step(0)) {
         door1 = value;
     }
-    function play_goat(int value) by(Role.HOST) id(2) depends_on1(1) {
+    function play_goat(uint value) perform_step(2) by(Role.HOST) only_after(step(1)) {
         goat = value;
     }
-    function play_door2(int value) by(Role.PLAYER) id(3) depends_on1(2) {
+    function play_door2(uint value) perform_step(3) by(Role.PLAYER) only_after(step(2)) {
         door2 = value;
     }
-    function reveal_car(int value) by(Role.HOST) id(4) depends_on1(3) {
-        open(value, hidden_car);
+    function reveal_car(uint value) perform_step(4) by(Role.HOST) only_after(step(3)) {
         car = value;
+    }
+
+}
+
+contract MontyHall is MontyHallDep {
+    function player_won() constant returns(bool) {
+        return goat == door1 || !open(car, hidden_car) || goat == car || door2 == car;
     }
 }
 
-contract STPG is Game {
+contract STPGDep is GameDep {
     uint256 hidden_choice1;
-    int choice1;
+    uint choice1;
     uint256 hidden_choice2;
-    int choice2;
+    uint choice2;
 
-    function hide_choice1(uint256 commitment) by(Role.HOST) id(0) depends_on0() {
+    function hide_choice1(uint256 commitment) perform_step(0) by(Role.HOST) first {
         hidden_choice1 = commitment;
     }
-    function hide_choice2(uint256 commitment) by(Role.PLAYER) id(1) depends_on0() {
+    function hide_choice2(uint256 commitment) perform_step(1) by(Role.PLAYER) first {
         hidden_choice2 = commitment;
     }
-    function reveal_choice1(int value) by(Role.HOST) id(2) depends_on1(1) {
-        open(value, hidden_choice2);
+    function reveal_choice1(uint value) perform_step(2) by(Role.HOST) only_after(step(1)) {
         choice2 = value;
     }
-    function reveal_choice2(int value) by(Role.HOST) id(3) depends_on1(0) {
-        open(value, hidden_choice1);
+    function reveal_choice2(uint value) perform_step(3) by(Role.PLAYER) only_after(step(0)) {
         choice1 = value;
+    }
+}
+
+contract STPG is STPGDep {
+    
+    function judge(uint choice1, uint choice2) internal returns (uint index);
+    
+    function payment() constant internal returns (uint[3]);
+    
+    uint256 constant PENALTY = 0; // = 10 ether;
+    uint256 constant COST = 0; // = 100 ether;
+
+    function player1_won() constant returns(bool) {
+        return !open(choice1, hidden_choice1) || open(choice1, hidden_choice1) && judge(choice1, choice2) == 0;
+    }
+}
+
+contract RockPaperScissors is STPG {
+    function judge(uint choice1, uint choice2) internal returns (uint index) {
+        uint8 diff = uint8((choice1 % 3 - choice2 % 3) % 3);
+        if (diff == 0) return 0;
+        if (diff == 1) return 1;
+        if (diff == 2) return 2;
+        throw;
+    }
+    
+    function payment() constant internal returns (uint[3]) {
+        return [COST + COST + PENALTY,
+                COST + 0    + PENALTY,
+                0    + 0    + PENALTY];
+    }
+}
+
+contract OddsAndEvens is STPG {
+    function judge(uint odd, uint even) internal returns (uint index) {
+        uint diff = (odd % 2 + even % 2);
+        return (diff % 2 == 0) ? 1 : 2;
+    }
+    
+    function payment() constant internal returns (uint[3]) {
+        return [0,
+                COST + 0    + PENALTY,
+                0    + 0    + PENALTY];
     }
 }
